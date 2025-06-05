e9493c74c96c5553016212c0471916ba95c5dc97
Felipe <lipejdc@gmail.com>
2025-06-05 14:25:16 +0200

    add form page and refactor bookmark.js

--- js/bookmarks.js
+++ js/bookmarks.js
@@ -3,9 +3,6 @@ const bookmarkIcon = document.querySelector('[data-js="bookmark-icon"]');
 const answerButton = document.querySelector('[data-js="answer-button"]');
 const answerText = document.querySelector('[data-js="answer-text"]');
 
-console.log(answerButton);
-console.log(answerText);
-
 //Bookmark
 const addBookmarkToCard = () => {
   bookmarkIcon.setAttribute("fill", "black");
@@ -17,12 +14,7 @@ const removeBookmarkFromCard = () => {
 
 bookmarkButton.addEventListener("click", () => {
   const currentFill = bookmarkIcon.getAttribute("fill");
-
-  if (currentFill === "none") {
-    addBookmarkToCard();
-  } else {
-    removeBookmarkFromCard();
-  }
+  currentFill === "none" ? addBookmarkToCard() : removeBookmarkFromCard();
 });
 
 //Answer button
